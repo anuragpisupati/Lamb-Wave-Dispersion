590d2fcf12b5c6df12074335340960e2034fbc74
9m2Codes <30552232+anuragpisupati@users.noreply.github.com>
2024-09-23 15:35:45 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ setup(
     version='0.0.0',
     description='"A module with tools to calculate and plot Lamb wave dispersion curves',
     long_description=readme,
-    author='Francisco Rotea',
-    author_email='francisco.rotea@gmail.com',
-    url='github.com/franciscorotea/Lamb-Wave-Dispersion/',
+    url='github.com/anuragpisupati/Lamb-Wave-Dispersion/',
     license=license,
     packages=find_packages(),
     install_requires=['numpy', 'scipy', 'matplotlib']
